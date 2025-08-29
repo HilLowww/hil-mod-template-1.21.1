836c6075aede9cd8d89c1ad807c2f53f2fac86be
HilLowww <hunter242224432@gmail.com>
2025-08-29 06:05:35 -0700

    COment

--- src/main/java/net/hil/hilmod/HilMod.java
+++ src/main/java/net/hil/hilmod/HilMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+// LLLLLLL
 public class HilMod implements ModInitializer {
 	public static final String MOD_ID = "hilmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
