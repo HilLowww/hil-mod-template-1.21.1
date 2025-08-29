95430f291fd95709eb20b2ce976c796bdd551d40
HilLowww <hunter242224432@gmail.com>
2025-08-29 05:45:18 -0700

    Initial commit

---
+++ src/main/java/net/hil/hilmod/HilModClient.java
@@ -0,0 +1,10 @@
+package net.hil.hilmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HilModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/hil/hilmod/HilModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.hil.hilmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HilModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
