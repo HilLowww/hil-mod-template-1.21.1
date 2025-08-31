17b3839759e37e418e69d8769bcfca694e223958
HilLowww <hunter242224432@gmail.com>
2025-08-31 07:09:22 -0700

    Tool Tips

--- src/main/java/net/hil/hilmod/item/ModItems.java
+++ src/main/java/net/hil/hilmod/item/ModItems.java
@@ -3,12 +3,18 @@ package net.hil.hilmod.item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.hil.hilmod.HilMod;
 import net.hil.hilmod.item.custom.ChiselItem;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+
+import java.util.List;
 
 public class ModItems {
     public static final Item CRYSTALITE = registerItem("crystalite", new Item(new Item.Settings()));
@@ -18,7 +24,19 @@ public class ModItems {
 
     public static final Item CAULIFLOWER = registerItem("cauliflower", new Item(new Item.Settings().food(ModFoodComponents.CAULIFLOWER)));
 
-    public static final Item ENDER_ROD = registerItem("ender_rod", new Item(new Item.Settings()));
+    public static final Item ENDER_ROD = registerItem("ender_rod", new Item(new Item.Settings()) {
+        @Override
+        public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+            if (Screen.hasShiftDown()) {
+                tooltip.add(Text.translatable("tooltip.hilmod.end_rod.shift_down"));
+            } else {
+                tooltip.add(Text.translatable("tooltip.hilmod.end_rod"));
+            }
+            super.appendTooltip(stack, context, tooltip, type);
+        }
+    });
+
+
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(HilMod.MOD_ID, name), item);
